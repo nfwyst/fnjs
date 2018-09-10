bcba73aa9b5155262db4a63274173d05e47c07e9
nfwyst <nfwyst@gmail.com>
2018-09-10 20:12:03 +0800

    add IO map

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var fnjs = (function() {
-  'use strict';
+  "use strict";
 
   // 构造函数
   function fnjs() {
@@ -7,28 +7,33 @@ var fnjs = (function() {
   }
 
   // 静态私有方法
-  ;(function(f) {
-    'use strict';
+  (function(f) {
+    "use strict";
     // 获取函数参数的个数
-    f.getParaNum = function (f) {
-      return f.toString().match(/\((.*)\)/)[1].split(',').length;
-    }
+    f.getParaNum = function(f) {
+      return f
+        .toString()
+        .match(/\((.*)\)/)[1]
+        .split(",").length;
+    };
   })(fnjs);
 
   // 实例方法
-  ;(function(p) {
-    'use strict';
+  (function(p) {
+    "use strict";
     // 初始化命名空间
-    p.init = function () {
-      this.name = 'fnjs';
-      this.version = '1.0.0';
-    }
+    p.init = function() {
+      this.name = "fnjs";
+      this.version = "1.0.0";
+    };
     // 判断输入的类型
     p.type = function(entry) {
-      return Object.prototype.toString.call(entry).replace(/^\[\w+ (\w+)\]$/, function(_, r) {
-        return r.toLowerCase();
-      });
-    }
+      return Object.prototype.toString
+        .call(entry)
+        .replace(/^\[\w+ (\w+)\]$/, function(_, r) {
+          return r.toLowerCase();
+        });
+    };
 
     /**
      * 将一个有多个参数的函数柯理化
@@ -37,14 +42,17 @@ var fnjs = (function() {
      * @param {Boolean} i 当前递归的层级是否是子层级
      * @returns Function
      */
-    p.curry = function (f, n, i) {
+    p.curry = function(f, n, i) {
       var type = p.type;
-      if(type(f) !== 'function') return function(f) { return f; };
-      var pn = n && type(n) === 'number' ? n : fnjs.getParaNum(f);
+      if (type(f) !== "function")
+        return function(f) {
+          return f;
+        };
+      var pn = n && type(n) === "number" ? n : fnjs.getParaNum(f);
       var argList = [];
 
-      return function (a) {
-        if(!i) {
+      return function(a) {
+        if (!i) {
           pn = fnjs.getParaNum(f);
           argList = [];
         }
@@ -54,18 +62,22 @@ var fnjs = (function() {
         } else {
           return f.apply(null, arguments);
         }
-        return p.curry(function () {
-          argList = argList.concat(Array.from(arguments));
-          argList.unshift(a);
-          var res = f.apply(null, argList);
-          if(!i) {
-            pn = fnjs.getParaNum(f);
-            argList = [];
-          }
-          return res;
-        }, pn, true);
-      }
-    }
+        return p.curry(
+          function() {
+            argList = argList.concat(Array.from(arguments));
+            argList.unshift(a);
+            var res = f.apply(null, argList);
+            if (!i) {
+              pn = fnjs.getParaNum(f);
+              argList = [];
+            }
+            return res;
+          },
+          pn,
+          true
+        );
+      };
+    };
     /**
      * 将多个函数组合起来
      * @param {Function} arguments
@@ -74,7 +86,7 @@ var fnjs = (function() {
     p.compose = function() {
       var args = Array.from(arguments);
       var reversed = false;
-      return function (arg) {
+      return function(arg) {
         var fns = args;
         if (!reversed) {
           fns = fns.reverse();
@@ -93,12 +105,12 @@ var fnjs = (function() {
         } else {
           return args[0](arg);
         }
-      }
-    }
+      };
+    };
 
     // a -> b -> a | b
-    p.add = p.curry(function (num, a) {
-      if(a.map) {
+    p.add = p.curry(function(num, a) {
+      if (a.map) {
         return a.map(v => v + num);
       } else {
         return a + num;
@@ -115,12 +127,12 @@ var fnjs = (function() {
     });
 
     // String -> Object -> any
-    p.prop = p.curry(function (property, obj) {
+    p.prop = p.curry(function(property, obj) {
       return obj[property];
     });
 
     // Function -> Array | Functor -> Array | Functor
-    p.map = p.curry(function (fn, datas) {
+    p.map = p.curry(function(fn, datas) {
       if (datas.map) {
         return datas.map(fn);
       } else {
@@ -129,67 +141,67 @@ var fnjs = (function() {
     });
 
     // Function -> string -> undefined
-    p.getJSON = p.curry(function (cb, url) {
+    p.getJSON = p.curry(function(cb, url) {
       var xhr = new XMLHttpRequest();
-      xhr.open('get', url);
-      xhr.setRequestHeader('Content-Type', 'json');
-      xhr.onload = function (res) {
+      xhr.open("get", url);
+      xhr.setRequestHeader("Content-Type", "json");
+      xhr.onload = function(res) {
         cb.call(res.target, res.target.responseText);
-      }
+      };
       xhr.send();
     });
 
     // HTMLElement -> string -> HTMLElement
-    p.setHtml = p.curry(function (ele, html) {
+    p.setHtml = p.curry(function(ele, html) {
       ele.innerHTML = html;
       return ele;
     });
 
     // HTMLElement -> string -> HTMLElement
-    p.appendHtml = p.curry(function (ele, html) {
-      ele.insertAdjacentHTML('beforeend', html);
+    p.appendHtml = p.curry(function(ele, html) {
+      ele.insertAdjacentHTML("beforeend", html);
       return ele;
     });
 
     // string -> any -> any
-    p.trace = p.curry(function (tag, pipeData) {
+    p.trace = p.curry(function(tag, pipeData) {
       console.log(tag, pipeData);
       return pipeData;
     });
 
     // any -> any
-    p.toJson = function (data) {
+    p.toJson = function(data) {
       return JSON.parse(data);
-    }
+    };
 
     // Function -> Object -> String -> undefined
-    p.postForm = p.curry(function (cb, data, url) {
+    p.postForm = p.curry(function(cb, data, url) {
       var xhr = new XMLHttpRequest();
-      xhr.open('post', url);
+      xhr.open("post", url);
       xhr.onload = function(res) {
         cb.call(res.target, res.target.responseText);
-      }
+      };
       var formData = new FormData();
-      for(var key in data) {
+      for (var key in data) {
         formData.append(key, data[key]);
       }
       xhr.send(formData);
     });
 
     // Function -> Object -> String -> undefined
-    p.postRaw = p.curry(function (cb, data, url) {
+    p.postRaw = p.curry(function(cb, data, url) {
       var xhr = new XMLHttpRequest();
-      xhr.open('post', url);
-      xhr.onload = function (res) {
+      xhr.open("post", url);
+      xhr.onload = function(res) {
         cb.call(res.target, res.target.responseText);
-      }
-      xhr.setRequestHeader('Content-Type', 'json');
+      };
+      xhr.setRequestHeader("Content-Type", "json");
       xhr.send(JSON.stringify(data));
     });
 
     // Function -> Array -> Array
     p.filter = p.curry(function(fn, datas) {
-      if(p.type(datas) === 'array') {
+      if (p.type(datas) === "array") {
         return datas.filter(fn);
       } else {
         return [fn(datas)];
@@ -199,96 +211,183 @@ var fnjs = (function() {
     // (String | Date, String) -> Object
     p.time = function(date, format) {
       this.date = date ? new Date(date) : new Date();
-      this.value = '';
-      if(!format) return this;
-      var formats = format.split('-');
-      formats = formats[0].length > 4 ? format.split('/') : formats;
-      for(var i = 0; i < formats.length; i++) {
+      this.value = "";
+      if (!format) return this;
+      var formats = format.split("-");
+      formats = formats[0].length > 4 ? format.split("/") : formats;
+      for (var i = 0; i < formats.length; i++) {
         var item = formats[i];
         switch (item) {
-          case 'YYYY':
+          case "YYYY":
             this.value += this.date.getFullYear();
             break;
-          case 'MM':
+          case "MM":
             this.value += this.date.getMonth();
-          case 'DD':
+          case "DD":
             this.value += this.date.getDate();
         }
       }
       return this;
-    }
+    };
+
+    p.split = p.curry(function(p, data) {
+      return data.split(p);
+    });
 
     // (String | Date, String) -> Object
     p.moment = function(date, format) {
       return new p.time(date, format);
-    }
+    };
 
     // time 实例
-    ;(function(p) {
-      'use strict';
+    (function(p) {
+      "use strict";
       // undefined -> Boolean
       p.isValid = function() {
         return Number.isFinite(this.date.getTime());
-      }
+      };
       // (a, b) -> c
-      p.diff = function (an, format) {
-        if(!an.date) return false;
+      p.diff = function(an, format) {
+        if (!an.date) return false;
         switch (format) {
-          case 'years':
-            return this.date.getFullYear() - an.date.getFullYear()
+          case "years":
+            return this.date.getFullYear() - an.date.getFullYear();
           default:
             return this.date.getFullYear() - an.date.getFullYear();
         }
-      }
-    })( p.time.prototype = p.time.prototype || {});
+      };
+    })((p.time.prototype = p.time.prototype || {}));
 
     // a -> b
     p.Container = function(d) {
       this.__value = d ? d : null;
-    }
+    };
 
     // a -> b
     p.Container.of = function(d) {
       return new p.Container(d);
-    }
+    };
+
+    // (a -> a) -> (a -> b) -> c -> a
+    p.either = p.curry(function(f, g, e) {
+      switch (e.constructor) {
+        case p.Container:
+          return g(e.__value);
+        case p.Message:
+          return f(e.__value);
+      }
+    });
+
+    // a -> a
+    p.id = function(x) {
+      return x;
+    };
+
+    // [a] | a -> a
+    p.head = function(a) {
+      try {
+        return a[0] ? a[0] : a;
+      } catch (err) {
+        return a;
+      }
+    };
+
+    // a -> Functor: b
+    p.query = function(selector) {
+      return new p.IO(function() {
+        return document.querySelectorAll(selector);
+      });
+    };
+
+    // [a] | a -> a
+    p.second = function(a) {
+      try {
+        return a[1] ? a[1] : a;
+      } catch (e) {
+        return a;
+      }
+    };
+
+    // a -> b -> c -> a
+    p.addEvent = p.curry(function(callback, type, el) {
+      if (el.length && Array.from(el).length) {
+        for (var i = 0; i < el.length; i++) {
+          var item = el[i];
+          item.addEventListener(type, callback);
+        }
+      } else {
+        el.addEventListener(type, callback);
+      }
+      return el;
+    });
+
+    // Functor: a -> b
+    p.IoValue = function(io) {
+      return io.unsafePerform();
+    };
+
+    // a => b
+    p.IO = function(f) {
+      this.unsafePerform = f;
+    };
+
+    // a -> Functor: b
+    p.IO.of = function(x) {
+      return new p.IO(function() {
+        return x;
+      });
+    };
 
     // Container 实例方法
-    ;(function(p) {
-      'use strict';
+    (function(p) {
+      "use strict";
       // undefined -> a
       p.isNothing = function() {
         return this.__value === null || this.__value === undefined;
-      }
+      };
       // (a -> b) -> c
-      p.map = function (f) {
-        return this.isNothing() ? this.constructor.of(null) : this.constructor.of(f(this.__value));
-      }
-    })(p.Container.prototype = p.Container.prototype || {});
+      p.map = function(f) {
+        return this.isNothing()
+          ? this.constructor.of(null)
+          : this.constructor.of(f(this.__value));
+      };
+    })((p.Container.prototype = p.Container.prototype || {}));
+
+    // IO 实例方法
+    (function(p1, p) {
+      "use strict";
+      p1.map = function(f) {
+        return new p.IO(
+          p.compose(
+            f,
+            this.unsafePerform
+          )
+        );
+      };
+    })((p.IO.prototype = p.IO.prototype || {}), p);
 
     // a -> b
-    p.Message = function (d) {
-      this.__value = d ? d : null
-    }
+    p.Message = function(d) {
+      this.__value = d ? d : null;
+    };
 
     // a -> b
-    p.Message.of = function (d) {
+    p.Message.of = function(d) {
       return new p.Message(d);
-    }
+    };
 
     // Message 实例方法
-    ;(function(p) {
-      'use strict';
+    (function(p) {
+      "use strict";
       // (a -> b) -> c
-      p.map = function (f) {
+      p.map = function(f) {
         return this;
-      }
-    })(p.Message.prototype = p.Message.prototype || {});
-
-  })(fnjs.prototype = fnjs.prototype || {});
+      };
+    })((p.Message.prototype = p.Message.prototype || {}));
+  })((fnjs.prototype = fnjs.prototype || {}));
 
   return new fnjs();
-}());
-
+})();
 
 // pure function
 var {
@@ -301,7 +400,9 @@ var {
   prop,
   map,
   concat,
-  trace
+  trace,
+  IO,
+  split
 } = fnjs;
 
 console.log(
@@ -311,26 +412,50 @@ console.log(
   }).map(prop("host"))
 );
 
-console.log(Container.of('haha').map(item => item + 1).map(item => item + 1));
+console.log(
+  Container.of("haha")
+    .map(item => item + 1)
+    .map(item => item + 1)
+);
 
-
-var getAge = curry(function (now, user) {
-  var birthdate = moment(user.birthdate, 'YYYY-MM-DD');
+var getAge = curry(function(now, user) {
+  var birthdate = moment(user.birthdate, "YYYY-MM-DD");
   if (!birthdate.isValid()) {
-    return Message.of('birthdate could not be parse');
-  } else
-    return Container.of(now.diff(birthdate, 'years'));
+    return Message.of("birthdate could not be parse");
+  } else return Container.of(now.diff(birthdate, "years"));
 });
 
-var exec = compose(concat('if you are right, you will be '), trace('加1后的结果'), add(1));
+var exec = compose(
+  concat("if you are right, you will be "),
+  trace("加1后的结果"),
+  add(1)
+);
 
-var ap = compose(map(console.log), map(exec), getAge(moment()));
+var ap = compose(
+  map(console.log),
+  map(exec),
+  getAge(moment())
+);
 
-console.log('======\n', ap({ birthdate: '2009-01-23' }));
-
+console.log("======\n", ap({ birthdate: "2009-01-23" }));
 
 var ap1 = compose(getAge(moment()));
 
-console.log(ap1({birthdate: 'error'}).map(console.log)); // should not work
+console.log(ap1({ birthdate: "error" }).map(console.log)); // should not work
 
-ap1({birthdate: '2000-01-01'}).map(trace('获取年龄的结果')).map(exec).map(console.log);
+ap1({ birthdate: "2000-01-01" })
+  .map(trace("获取年龄的结果"))
+  .map(exec)
+  .map(console.log);
+
+var ioObj = IO.of({ name: { name: { name: "n,a,m,e" } } });
+
+var ress = ioObj
+  .map(function(o) {
+    return o.name;
+  })
+  .map(prop("name"))
+  .map(prop("name"))
+  .map(split(","));
+
+console.log(ress.unsafePerform());
